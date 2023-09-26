efd43c47c936962d0486fed6ef872c7e3bb32929
github masato4 <yags6bok2@gmail.com>
2023-09-26 10:33:54 +0900

    done11

--- pages/_app.js
+++ pages/_app.js
@@ -6,14 +6,54 @@ import 'prismjs/plugins/line-numbers/prism-line-numbers.css';
 import SEO from '../next-seo.config';
 import { DefaultSeo } from 'next-seo';
 
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import * as gtag from '../lib/gtag';
+import { GA_TRACKING_ID } from '../lib/gtag'; // トラッキング ID を追加
+
+import Script from "next/script";
+
 
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, []);
+
+
+
+
   return (
-    <Layout>
-      <DefaultSeo {...SEO} />
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Layout>
+        <DefaultSeo {...SEO} />
+        <Component {...pageProps} />
+      </Layout>
+      <Script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag() { dataLayer.push(arguments); }
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}');
+          `,
+        }}
+      />
+    </>
   );
 }
 
